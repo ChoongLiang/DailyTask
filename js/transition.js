847a17400fb6e26926269cb53bec4b24953c629d
choongliang <choongli@buffalo.edu>
2019-04-02 17:29:00 -0400

    Day 2
    **Updates:**
    HTML:
    - Added title
    - Added jquery and link to transition js file
    - Added seconds for fade in effect
    
    CSS:
    - Body background color updated to white, font-family changed to Lato, and font size is now 15px
    - Removed background image properties
    - Changed all urgent and important position to relative, and %
    - Drawed both horizontal and vertical line from hr
    - Hr properties and position added
    - Added fade in animation for urgent and important text
    - Fade in animation works on all browser
    
    Javascript:
    - New file named transition.js added
    
    **Removed:**
    - background.png, background.psd
    
    **Moved*:*
    - Categorized files to their corresponding folder, such as css and js files.
    
    **Bug:**
    [ ] Hr line animation is not working... no reason... The code works on codepen but not on browser.
    Error: "Transition.js" not functioning
    Possible cause:
    - Manifest.json file needed declaration of js file
    - Transition.js add "animation" when page is ready, so maybe new page is not "ready", which caused the js file to not function

---
+++ js/transition.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+    setTimeout(function(){
+        $('.expand').addClass('grow');
+    }, 275);
+});
+
+jQuery(document).ready(function($){
+    setTimeout(function(){
+        $('.expandVertical').addClass('growVertical');
+    }, 275);
+});
+
