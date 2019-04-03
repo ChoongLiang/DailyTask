eeedafaf237667d099789231f284971a2f104da5
choongliang <choongli@buffalo.edu>
2019-04-03 10:38:42 -0400

    Bug fixes
    Updates:
    
    Css:
    - fixes width and height
    
    Additional files:
    - Downloaded jquery locally to avoid chrome security content whitelisting

--- js/transition.js
+++ js/transition.js
@@ -1,12 +1,14 @@
 jQuery(document).ready(function($){
     setTimeout(function(){
         $('.expand').addClass('grow');
+        console.log("expand working")
     }, 275);
 });
 
 jQuery(document).ready(function($){
     setTimeout(function(){
         $('.expandVertical').addClass('growVertical');
+        console.log("expand vertical working")
     }, 275);
 });
 
