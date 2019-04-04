ab057d813b7ad2617543513b333ba20cba529c4c
choongliang <choongli@buffalo.edu>
2019-04-04 16:43:25 -0400

    Updates
    - Working drag and drop functionality

--- js/drag.js
+++ js/drag.js
@@ -1,8 +1,69 @@
+// credits: https://www.html5rocks.com/en/tutorials/dnd/basics/
+
+// Drag element
+var dragSrcEl = null;
+
 function handleDragStart(e) {
   this.style.color = 'red';  // this / e.target is the source node.
+
+  dragSrcEl = this;
+
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
 }
 
-// var cols = document.querySelectorAll('#columns .column');
-// [].forEach.call(cols, function(col) {
-//   col.addEventListener('dragstart', handleDragStart, false);
-// });
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault(); // Necessary. Allows us to drop.
+  }
+
+  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+  return false;
+}
+
+function handleDragEnter(e) {
+  // this / e.target is the current hover target.
+  this.classList.add('over');
+  console.log(this.classList);
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');  // this / e.target is previous target element.
+  console.log(this.classList);
+}
+
+function handleDrop(e) {
+  // this / e.target is current target element.
+
+  if (e.stopPropagation) {
+    e.stopPropagation(); // stops the browser from redirecting.
+  }
+  if (dragSrcEl != this) {
+    // Set the source column's HTML to the HTML of the column we dropped on.
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+    //console.log('this: ', this.innerHTML.classList);
+    //this.innerHTML.classList.add('listedItem');
+  }
+
+  return false;
+}
+
+function handleDragEnd(e) {
+  // this/e.target is the source node.
+
+  [].forEach.call(list, function (item) {
+    item.classList.remove('over');
+  });
+}
+
+var list = document.querySelectorAll('.list');
+[].forEach.call(list, function(item) {
+  item.addEventListener('dragstart', handleDragStart, false);
+  item.addEventListener('dragenter', handleDragEnter, false);
+  item.addEventListener('dragover', handleDragOver, false);
+  item.addEventListener('dragleave', handleDragLeave, false);
+  item.addEventListener('drop', handleDrop, false);
+  item.addEventListener('dragend', handleDragEnd, false);
+});
